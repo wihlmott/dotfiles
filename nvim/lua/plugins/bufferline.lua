e86a5918a6b4632c1a5891a4ae845813b02f0b91
Wihlmott <wit.valentine@gmail.com>
2026-05-17 14:56:10 +0200

    starship config added

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
     "akinsho/bufferline.nvim",
     version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 
     opts = {
         options = {
--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,13 +1,16 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-      },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+            },
+            format_on_save = {
+                timeout_ms = 1000,
+                lsp_fallback = true,
+            },
+        },
     },
-  },
 }
